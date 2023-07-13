4baf8d8a723adbbb4ed3eb2a9b404132c4408814
Victor Longui Pereira <longuivictor@gmail.com>
2023-07-13 14:26:58 -0300

    adição de recipe-details

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ def home(request):
     return render(request, 'recipes/pages/home.html')
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/recipe-view.html')
 
 
 
