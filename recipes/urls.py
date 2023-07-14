02d138e43b28f873b79440aa9f19da04dbaf593f
Victor Longui Pereira <longuivictor@gmail.com>
2023-07-14 14:18:17 -0300

    injetando caminhos e urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+#
+app_name = 'sabores-do-mundo'
 
 urlpatterns = [
-    path('', views.home), # home
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"), # home
+    path('recipes/<int:id>/', views.recipe, name="receita"),
 
 
 ]
